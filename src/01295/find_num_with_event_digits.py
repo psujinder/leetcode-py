e3b0edc6d7b33925e2b9ac07ecb21a827597eae6
Sujinder Pothula <psujinder@hotmail.com>
2024-09-08 10:27:41 -0700

    solved leetcode 01295

---
+++ src/01295/find_num_with_event_digits.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    """
+    This class provides a solution to the problem of finding how many numbers
+    in a list contain an even number of digits.
+
+    Time Complexity: O(n * d)
+        - where 'n' is the number of elements in the list and 'd' is the number of digits in the largest number (since we convert each number to a string to check its length).
+
+    Space Complexity: O(1)
+        - we use constant additional space regardless of the size of the input list.
+    """
+
+    def findNumbers(self, nums: List[int]) -> int:
+
+        return sum(1 for num in nums if self._containsEvenDigits(num))
+
+    def _containsEvenDigits(self, num):
+
+        return len(str(num)) % 2 == 0
---
+++ src/01295/find_num_with_event_digits_test.py
@@ -0,0 +1,14 @@
+import pytest
+from find_num_with_event_digits import Solution
+
+
+@pytest.fixture
+def solution():
+    return Solution()
+
+
+@pytest.mark.parametrize(
+    "nums,count", [([12, 345, 2, 6, 7896], 2), ([555, 901, 482, 1771], 1)]
+)
+def test_find_num_with_even_digits(solution, nums, count):
+    assert solution.findNumbers(nums) == count
