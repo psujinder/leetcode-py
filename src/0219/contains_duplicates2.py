18f21f93adbde616400c7fe390f6165254e7d9e2
Sujinder Pothula <psujinder@hotmail.com>
2025-04-12 16:44:35 -0700

    solved more leetcode problems

--- src/0219/contains_duplicates2.py
+++ src/0219/contains_duplicates2.py
@@ -4,6 +4,18 @@ from typing import List
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
 
+        last_seen = {}
+
+        for i, num in enumerate(nums):
+            if num in last_seen and i - last_seen[num] <= k:
+                return True
+            else:
+                last_seen[num] = i
+
+        return False
+
+    def containsNearbyDuplicateSet(self, nums: List[int], k: int) -> bool:
+
         seen = set()
 
         for i in range(len(nums)):
